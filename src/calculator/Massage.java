a250e7c71a8cb52e54f54837c07de5c6d58d8bb0
Ara Gasparyan <ara.gasparyan87@gmail.com>
2022-02-03 14:45:51 +0400

    Translate messages to English

--- src/calculator/Massage.java
+++ src/calculator/Massage.java
@@ -4,10 +4,10 @@ package calculator;
  * @author Ara Gasparyan
  */
 public enum Massage {
-    Massage1Arm("չգոյ",1,"AM"),
-    Massage2Arm("Մուտքագրուած թիւն գոյութիւն չունի",2,"AM"),
-    Massage3Arm("Մուտագրուէք չորս թուանշանից ոչ ավել թուանշան պարոնակող թիւ",3,"AM"),
-    Massage4Arm("Զրոյի վրայ բաժանումն խստիւ արգելվում է",4,"AM");
+    Massage1Arm("Does not exist",1,"EN"),
+    Massage2Arm("Not a number",2,"EN"),
+    Massage3Arm("The maximal number of digits of the natural number should be 4",3,"EN"),
+    Massage4Arm("Division by zero",4,"EN");
 
     private final String text;
     private final int id;
